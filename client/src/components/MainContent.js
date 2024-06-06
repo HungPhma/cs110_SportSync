272aa9acd53e2fbc33e888818284dfbc6b59ff57
HungPham097 <141068446+HungPhma@users.noreply.github.com>
2024-06-06 16:07:47 -0700

    update banner, header, follow even, maincontent

---
+++ client/src/components/MainContent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MainContent = () => {
+  return (
+    <main className="App-main">
+        <div>
+            <h2>Main Home Page</h2>
+        </div>
+    </main>
+  );
+};
+
+export default MainContent;
