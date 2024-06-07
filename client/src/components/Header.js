22463119ec4cc160c52cfb2fb65f4a854459d254
HungPham097 <141068446+HungPhma@users.noreply.github.com>
2024-06-07 02:51:03 -0700

    add event plan, friend, and history

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../Photo/project2.png'; // Ensure the path to your logo is correct
 import profilePic from '../Photo/user-default.png'; // Ensure the path to your profile picture is correct
 import notificationIcon from '../Photo/notification.svg'; // Ensure the path to your notification icon is correct
@@ -7,21 +7,73 @@ import '../App.css';
 
 const Header = () => {
   const [isSearchVisible, setIsSearchVisible] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [timeoutId, setTimeoutId] = useState(null);
 
   const handleSearchClick = () => {
     setIsSearchVisible(true);
   };
 
+  const handleInputChange = (e) => {
+    setSearchQuery(e.target.value);
+    // Clear the previous timeout
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+  };
+
+  const startTimeout = () => {
+    const newTimeoutId = setTimeout(() => {
+      setIsSearchVisible(false);
+    }, 5000);
+    setTimeoutId(newTimeoutId);
+  };
+
+  useEffect(() => {
+    if (isSearchVisible) {
+      startTimeout();
+
+      // Cleanup function to clear the timeout when the component unmounts or when searchQuery changes
+      return () => {
+        if (timeoutId) {
+          clearTimeout(timeoutId);
+        }
+      };
+    }
+  }, [isSearchVisible]);
+
+  useEffect(() => {
+    // Clear the timeout and reset it when searchQuery changes
+    if (isSearchVisible) {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+      startTimeout();
+    }
+  }, [searchQuery]);
+
   return (
     <div className="App-header">
       <div className="left-section">
         {isSearchVisible ? (
-          <input type="text" placeholder="Search..." className="search-bar" />
-        ) : (<img src={searchIcon} alt="Search" className="search-icon" onClick={handleSearchClick} />)
-        }
+          <input
+            type="text"
+            placeholder="Search..."
+            className="search-bar"
+            value={searchQuery}
+            onChange={handleInputChange}
+            onBlur={() => {
+              if (!searchQuery) {
+                setIsSearchVisible(false);
+              }
+            }}
+          />
+        ) : (
+          <img src={searchIcon} alt="Search" className="search-icon" onClick={handleSearchClick} />
+        )}
       </div>
       <div className="center-section">
-          <img src={logo} alt="Logo" className="App-logo" />
+        <img src={logo} alt="Logo" className="App-logo" />
       </div>
       <div className="right-section">
         <img src={notificationIcon} alt="Notifications" className="notification-icon" />
