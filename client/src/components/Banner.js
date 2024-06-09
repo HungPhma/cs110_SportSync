66063276868f49feabae9e82f88b9ff5a8ee93ee
djordan015 <69061900+djordan015@users.noreply.github.com>
2024-06-08 20:23:27 -0700

    mock up of front end and updates.

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -11,30 +11,30 @@ const Banner = () => {
         <div className="section-title">
           <img src={planIcon} alt="Plan Event Icon" className="icon" /> <h5>Plan Event</h5>
         </div>
+        {/* <div>event 1</div>
         <div>event 1</div>
         <div>event 1</div>
-        <div>event 1</div>
-        <div>event 1</div>
+        <div>event 1</div> */}
       </div>
       {/* <hr/> */}
       <div className="banner-section">
         <div className="section-title">
           <img src={friendIcon} alt="Friend Event Icon" className="icon" /> <h5>Friends</h5>
         </div>
+        {/* <div>friend 1</div>
         <div>friend 1</div>
         <div>friend 1</div>
-        <div>friend 1</div>
-        <div>friend 1</div>
+        <div>friend 1</div> */}
       </div>
       {/* <hr/> */}
       <div className="banner-section">
          <div className="section-title">
           <img src={historyIcon} alt="Friend Event Icon" className="icon" /> <h5>Event History</h5>
         </div>
+        {/* <div>history 1</div>
         <div>history 1</div>
         <div>history 1</div>
-        <div>history 1</div>
-        <div>history 1</div>
+        <div>history 1</div> */}
       </div>
     </div>
   );
--- client/src/components/FollowEvent.js
+++ client/src/components/FollowEvent.js
@@ -4,8 +4,34 @@ const Followevent = () => {
   return (
     <div className="App-banner">
         <div>
-            Event
+          Happening Now
         </div>
+        <br></br>
+        <p>Event A</p>
+        <p>Event B</p>
+        <p>Event C</p>
+        <p>Event D</p>
+        <br></br>
+
+        <div>
+          <strong>My </strong>Upcoming Events
+        </div>
+        <br></br>
+        <p>Event A</p>
+        <p>Event B</p>
+        <p>Event C</p>
+        <p>Event D</p>
+        <br></br>
+
+        <div>
+          Past Events
+        </div>
+        <br></br>
+        <p>Event A</p>
+        <p>Event B</p>
+        <p>Event C</p>
+        <p>Event D</p>
+
     </div>
   );
 };
